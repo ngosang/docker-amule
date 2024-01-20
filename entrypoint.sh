e5ea76752146a23d85ef94f244287d6dd25e2e78
ngosang <ngosang@hotmail.es>
2024-01-20 14:44:56 +0100

    Fix MOD_AUTO_SHARE to re-scan directories after aMule restart

--- entrypoint.sh
+++ entrypoint.sh
@@ -55,6 +55,7 @@ mod_auto_share() {
         printf "[MOD_AUTO_SHARE] Sharing the following directories with sub-directories: %s ... You can disable this mod with MOD_AUTO_SHARE_ENABLED=false\n" "$MOD_AUTO_SHARE_DIRECTORIES"
         SHAREDDIR_CONF=${AMULE_HOME}/shareddir.dat
         printf "/incoming\n" > "$SHAREDDIR_CONF"
+        iter=""
         IN="$MOD_AUTO_SHARE_DIRECTORIES"
         while [ "$IN" != "$iter" ] ;do
             iter=${IN%%;*}
